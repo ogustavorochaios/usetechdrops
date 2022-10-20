5b16f4fd24bb7ff9eaf868ae53f5686ee30d0042
Gustavo Rocha <gustavo.rocha@usemobile.com.br>
2022-10-20 16:36:37 -0300

    add example

---
+++ Example/Example/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  Example
+//
+//  Created by Gustavo Rocha on 20/10/22.
+//
+
+import UIKit
+import UseTechDrops
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        Validator.sayHello()
+    }
+}
+
---
+++ Example/Pods/Target Support Files/UseTechDrops/UseTechDrops-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_UseTechDrops : NSObject
+@end
+@implementation PodsDummy_UseTechDrops
+@end

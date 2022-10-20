621f3f18203c1224ed0ccf9ac567b25e988b11d3
Gustavo Rocha <gustavo.rocha@usemobile.com.br>
2022-10-20 16:20:08 -0300

    add feature

---
+++ UseTechDrops/Extension/Validator.swift
@@ -0,0 +1,14 @@
+//
+//  Validator.swift
+//  UseTechDrops
+//
+//  Created by Gustavo Rocha on 20/10/22.
+//
+
+import Foundation
+
+public struct Validator {
+    public static func sayHello() {
+        print("Hello!!")
+    }
+}
